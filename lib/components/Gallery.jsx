aa050b19ff8b0334309b582161491f12b5296727
Maia Stone <maiadov@gmail.com>
2016-12-27 10:52:56 -0700

    WIP designing grid

--- lib/components/Gallery.jsx
+++ lib/components/Gallery.jsx
@@ -1,23 +1,50 @@
 import React, { Component } from 'react';
-import photos from '../photos.js'
+import Masonry from 'react-masonry-component';
+import photos from '../photos.js';
+import commercial from '../commercial.js';
 
-class Gallery extends React.Component {
+const masonryOptions = {
+  transitionDuration: 0,
+};
 
-
+const Gallery = React.createClass({
   render() {
-    const imgCard = photos.map((photo) =>
-      <div className ='imgCard' key={photo.id}>
-        <h1>{photo.title}</h1>
-        <img className='photo' src={photo.imgLink}></img>
-      </div>
+    const childElements = photos.map(function(photo) {
+      return (
+        <li className='' key={photo.id}>
+          <img
+            className=''
+            src={photo.imgLink} />
+        </li>
       );
+    });
+    const childElements2 = commercial.map(function(photo) {
+      return (
+        <li className='' key={photo.id}>
+          <img
+            className=''
+            src={photo.imgLink} />
+        </li>
+      );
+    });
+
 
     return (
-      <div>
-        {imgCard}
+      <div className=''>
+        <div className=''></div>
+        <Masonry
+          className={''}
+          elementType={'ul'}
+          options={masonryOptions}
+          disableImagesLoaded={false}
+          updateOnEachImageLoad={false}
+        >
+        {childElements}
+        {childElements2}
+        </Masonry>
       </div>
     );
-  }
-}
+  },
+});
 
 module.exports = Gallery;
