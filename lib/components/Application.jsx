3ca581fe3e46f06166c900c8a716f31d061c30b8
Maia Stone <maiadov@gmail.com>
2017-01-06 22:22:05 -0700

    Add functionality modal to open  at currentImage

--- lib/components/Application.jsx
+++ lib/components/Application.jsx
@@ -17,19 +17,19 @@ class Application extends React.Component {
   toggleModal(e, index) {
     this.setState({
       modalIsOpen: !this.state.modalIsOpen,
-      currentImage: index - 1,
-    });
-  }
-
-  clickPrev(e, index) {
-    this.setState({
-      currentImage: index - 2,
-    });
-  }
-
-  clickNext(e, index) {
-    this.setState({
       currentImage: index,
+    });
+  }
+
+  clickPrev() {
+    this.setState({
+      currentImage: this.state.currentImage - 1,
+    });
+  }
+
+  clickNext() {
+    this.setState({
+      currentImage: this.state.currentImage + 1,
     });
   }
 
